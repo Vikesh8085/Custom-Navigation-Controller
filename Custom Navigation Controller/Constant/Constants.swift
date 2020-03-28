b9b266a2dc3ab623f8ba3a0a337cb05d3ff8008a
Vikesh <Vikesh.prasad@mobiotics.com>
2020-03-28 23:59:04 +0530

    Large Title added to Navigation Controller

---
+++ Custom Navigation Controller/Constant/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Custom Navigation Controller
+//
+//  Created by Vikesh Prasad on 28/03/20.
+//  Copyright © 2020 Mobiotics. All rights reserved.
+//
+
+import Foundation
+struct Constant {
+    static let blueScreen: String = "Blue Screen"
+    static let pinkScreen: String = "Pink Screen"
+}
---
+++ Custom Navigation Controller/Controllers/BlueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BlueViewController.swift
+//  Custom Navigation Controller
+//
+//  Created by Vikesh Prasad on 28/03/20.
+//  Copyright © 2020 Mobiotics. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = Constant.blueScreen
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
